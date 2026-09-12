c30ca59980d1ca5713d66052aa29a5d9c319c398
Bikash Dalapati <bikashdalapati09@gmail.com>
2026-09-12 17:43:16 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import Billing from "./pages/Billing"
 import {Toaster} from "react-hot-toast"
 
 export const serverURL = "https://webmate-ai-server.onrender.com";
-export const CLIENT_URL = "http://localhost:5173";
+export const CLIENT_URL = "https://webmate-ai-hepn.onrender.com";
 
 const App = () => {
   const [user, setUser] = useState(null);
